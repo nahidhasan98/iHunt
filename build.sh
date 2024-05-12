66478a1bb96323d7bfeb37e378c3a781c7584c2b
Nahid Hasan <nahid.hasan@brotecs.com>
2024-05-12 10:26:32 +0600

    added resources

--- build.sh
+++ build.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 # windows
-GOOS=windows go build -o wazuh/windows/ar_file_list_windows.exe go_src/ar_file_list_windows.go
 GOOS=windows go build -o wazuh/windows/master_ar_windows.exe go_src/master_ar_windows.go
+GOOS=windows go build -o wazuh/windows/ar_file_list_windows.exe go_src/ar_file_list_windows.go
 GOOS=windows go build -o wazuh/windows/ar_file_delete_windows.exe go_src/ar_file_delete_windows.go
 
 # macos
 GOOS=darwin go build -o wazuh/macos/master_ar_mac go_src/master_ar_mac.go
+GOOS=darwin go build -o wazuh/macos/ar_file_list_mac go_src/ar_file_list_mac.go
 GOOS=darwin go build -o wazuh/macos/ar_file_delete_mac go_src/ar_file_delete_mac.go
 
 # linux
 go build -o wazuh/linux/master_ar_linux go_src/master_ar_linux.go
+go build -o wazuh/linux/ar_file_list_linux go_src/ar_file_list_linux.go
 go build -o wazuh/linux/ar_file_delete_linux go_src/ar_file_delete_linux.go
 
 echo "Built successfully."
