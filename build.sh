eb384e5e7a11839fdd467356680c28dcb90bf1c9
Nahid Hasan <nahid.hasan@brotecs.com>
2024-05-12 10:36:45 +0600

    modified built script

--- build.sh
+++ build.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-# windows
-GOOS=windows go build -o wazuh/windows/master_ar_windows.exe go_src/master_ar_windows.go
-GOOS=windows go build -o wazuh/windows/ar_file_list_windows.exe go_src/ar_file_list_windows.go
-GOOS=windows go build -o wazuh/windows/ar_file_delete_windows.exe go_src/ar_file_delete_windows.go
+# linux
+GOOS=linux go build -o wazuh/linux/master_ar_linux go_src/master_ar_linux.go
+GOOS=linux go build -o wazuh/linux/ar_file_list_linux go_src/ar_file_list_linux.go
+GOOS=linux go build -o wazuh/linux/ar_file_delete_linux go_src/ar_file_delete_linux.go
 
 # macos
 GOOS=darwin go build -o wazuh/macos/master_ar_mac go_src/master_ar_mac.go
 GOOS=darwin go build -o wazuh/macos/ar_file_list_mac go_src/ar_file_list_mac.go
 GOOS=darwin go build -o wazuh/macos/ar_file_delete_mac go_src/ar_file_delete_mac.go
 
-# linux
-go build -o wazuh/linux/master_ar_linux go_src/master_ar_linux.go
-go build -o wazuh/linux/ar_file_list_linux go_src/ar_file_list_linux.go
-go build -o wazuh/linux/ar_file_delete_linux go_src/ar_file_delete_linux.go
+# windows
+GOOS=windows go build -o wazuh/windows/master_ar_windows.exe go_src/master_ar_windows.go
+GOOS=windows go build -o wazuh/windows/ar_file_list_windows.exe go_src/ar_file_list_windows.go
+GOOS=windows go build -o wazuh/windows/ar_file_delete_windows.exe go_src/ar_file_delete_windows.go
 
 echo "Built successfully."
